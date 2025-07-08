0be0f8d170050db0a1cdd087401ba5967d655b30
Omodolapo20 <122370795+Omodolapo20@users.noreply.github.com>
2025-07-08 14:19:30 +0100

    prod

--- src/pages/dashboard/order/orderList.jsx
+++ src/pages/dashboard/order/orderList.jsx
@@ -21,15 +21,10 @@ function OrderList() {
       <div className="inline-block min-w-full  rounded-lg overflow-hidden">
         <div className="flex items-center justify-between p-4 mt-3">
           <h1 className="text-3xl font-semibold">
-            Order <span className="text-black-400"></span>
+            Payment<span className="text-black-400"></span>
           </h1>
           <div className='flex gap-3'>
-            <Button
-              label="Cancel"
-              variant="transparent"
-              size="lg"
-              className="text-sm w-[150px]"
-            />
+
             <Link to={'/payment_statistics'}> <Button
               label="View payment statistics"
               variant="solid"
